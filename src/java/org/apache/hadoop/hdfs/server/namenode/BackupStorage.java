d63661507a4d930368b731a5a5097208672e5021
Todd Lipcon <todd@cloudera.com>
2010-04-12 15:23:00 -0700

    HDFS-1462. Refactor edit log loading from writing
    Put ops in an Ops inner class
    
    Refactor edit log loading into FSEditLogLoader
    
    Reapply HDFS-1014, got lost in the merge
    
    Conflicts:
    
    	src/java/org/apache/hadoop/hdfs/server/namenode/FSEditLog.java

--- src/java/org/apache/hadoop/hdfs/server/namenode/BackupStorage.java
+++ src/java/org/apache/hadoop/hdfs/server/namenode/BackupStorage.java
@@ -43,6 +43,9 @@ public class BackupStorage extends FSImage {
 
   /** Backup input stream for loading edits into memory */
   private EditLogBackupInputStream backupInputStream;
+  
+  private final FSEditLogLoader logLoader;
+  
   /** Is journal spooling in progress */
   volatile JSpoolState jsState;
 
@@ -57,6 +60,7 @@ public class BackupStorage extends FSImage {
   BackupStorage() {
     super();
     jsState = JSpoolState.OFF;
+    logLoader = new FSEditLogLoader(namesystem);
   }
 
   @Override
@@ -214,7 +218,7 @@ public class BackupStorage extends FSImage {
           waitSpoolEnd();
           // update NameSpace in memory
           backupInputStream.setBytes(data);
-          editLog.loadEditRecords(getLayoutVersion(),
+          logLoader.loadEditRecords(getLayoutVersion(),
                     backupInputStream.getDataInputStream(), true);
           getFSNamesystem().dir.updateCountForINodeWithQuota(); // inefficient!
           break;
@@ -334,11 +338,11 @@ public class BackupStorage extends FSImage {
       // load edits.new
       EditLogFileInputStream edits = new EditLogFileInputStream(jSpoolFile);
       DataInputStream in = edits.getDataInputStream();
-      numEdits += editLog.loadFSEdits(in, false);
+      numEdits += logLoader.loadFSEdits(in, false);
   
       // first time reached the end of spool
       jsState = JSpoolState.WAIT;
-      numEdits += editLog.loadEditRecords(getLayoutVersion(), in, true);
+      numEdits += logLoader.loadEditRecords(getLayoutVersion(), in, true);
       getFSNamesystem().dir.updateCountForINodeWithQuota();
       edits.close();
     }
