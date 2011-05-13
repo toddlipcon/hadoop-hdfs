b46cc9366a4d350bd48122d862ecf7ee19cde7d5
Todd Lipcon <todd@cloudera.com>
2011-05-12 23:16:50 -0700

    Fix OIV to new version numbers

--- src/java/org/apache/hadoop/hdfs/tools/offlineEditsViewer/EditsLoaderCurrent.java
+++ src/java/org/apache/hadoop/hdfs/tools/offlineEditsViewer/EditsLoaderCurrent.java
@@ -41,7 +41,7 @@ class EditsLoaderCurrent implements EditsLoader {
 
   private static int[] supportedVersions = {
       -18, -19, -20, -21, -22, -23, -24,
-      -25, -26, -27, -28, -30, -31, -32, -33, -34, -35, -36 };
+      -25, -26, -27, -28, -30, -31, -32, -33, -34, -35, -36, -37 };
 
   private EditsVisitor v;
   private int editsVersion = 0;
